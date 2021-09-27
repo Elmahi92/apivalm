d7def75c7375fb42f9e74e4f1357134e7b106638
Mohamed Ali <mohamedelmahi92@outlook.com>
2021-09-28 01:42:31 +0200

    changes in system.file

--- R/runExample.R
+++ R/runExample.R
@@ -1,7 +1,14 @@
+#' apivlam shiny result
+#' @examples
+#' ## Only run this example in interactive R sessions
+#' if (interactive()) {
+#'   runExample()
+#' }
+#' @export
 runExample <- function() {
-  appDir <- system.file("shiny-examples", "apivalm", package = "valmyndigheten_get")
+  appDir <- system.file("shiny-examples", "myapp", package = "apivalm")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `valmyndigheten_get`.", call. = FALSE)
+    stop("Could not find example directory. Try re-installing `apivalm`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
